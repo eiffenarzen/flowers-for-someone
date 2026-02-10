295d35ce5b03a98b50f5cbc7bef68388643ea578
zen <eiffenarjen26@gmail.com>
2026-02-10 13:28:45 +0700

    Update text content in index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `For My Fineshit!`.split('')
+const text = `For U My Fineshit!`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
